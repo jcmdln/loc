9ab10f85c06f2fcba4acd1ebb925a23ccb443f67
jcmdln <jcmdln@gmail.com>
2020-05-22 15:59:41 -0400

    Iterate over file by buffer; catch *previous = NULL

--- src/parse.c
+++ src/parse.c
@@ -15,41 +15,42 @@ loc_parse(struct langs *lang, int file, char *buffer)
 
 	++lang->count.files;
 
-	len = read(file, buffer, MAXBSIZE);
+	while ((len = read(file, buffer, MAXBSIZE)) > 0) {
+		for (character = buffer; len--; ++character) {
+			switch (*character) {
+			case '\n':
+				if (previous != NULL && *previous == '\n') {
+					++lang->count.blank;
+				} else if (in_comment) {
+					++lang->count.comment;
+				} else {
+					++lang->count.code;
+				}
 
-	for (character = buffer; len--; ++character) {
-		switch (*character) {
-		case '\n':
-			if (*previous == '\n') {
-				++lang->count.blank;
-			} else if (in_comment) {
-				++lang->count.comment;
-			} else {
-				++lang->count.code;
-			}
-
-			break;
-
-		case '*':
-			if (!in_comment && *previous == '/')
-				in_comment = 1;
-
-			break;
-
-		case '/':
-			if (previous == NULL)
 				break;
 
-			if (in_comment && *previous == '*')
-				in_comment = 0;
+			case '*':
+				if (!in_comment && *previous == '/')
+					in_comment = 1;
 
-			if (*previous == '*' || *previous == '/')
-				++lang->count.comment;
+				break;
 
-			break;
+			case '/':
+				if (previous == NULL)
+					break;
+
+				if (in_comment && *previous == '*')
+					in_comment = 0;
+
+				if (*previous == '*' ||
+				    *previous == '/')
+					++lang->count.comment;
+
+				break;
+			}
+
+			previous = character;
 		}
-
-		previous = character;
 	}
 
 	return 0;
